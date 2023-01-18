c99197d2478d8a61e69ac856dc32e227425176d6
Adam B <bela1003@usherbrooke.ca>
2023-01-18 15:21:06 -0500

    real mTLS

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const options = {
     key: fs.readFileSync(__dirname + '/../../cert/server/server-key.pem'),
     cert: fs.readFileSync(__dirname + '/../../cert/server/server-cert.pem'),
+    ca: fs.readFileSync(__dirname + '/../../autorite/autorite.cer'),
     requestCert: true,
     rejectUnauthorized: false,
     passphrase: 'gei761'
@@ -17,6 +18,10 @@ http.createServer((req, res) => {
 ).listen(8080);
 
 https.createServer(options, (req, res) => {
+    if (!req.client.authorized) {
+        res.writeHead(401);
+        return res.end('Invalid client certificate authentication.');
+      }
     res.writeHead(200);
     res.end('Hello Https');
 }
